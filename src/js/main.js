96e08b31d1d47c88615b547948a61068e1ed57ad
BorisBoko <borispetrosyans8@gmail.com>
2021-01-29 16:39:37 +0400

    29.01.2021

--- src/js/main.js
+++ src/js/main.js
@@ -11,4 +11,10 @@ $('div').click(function() {
     console.log($(this).index());
 })
 
+// console.log($('div').eq(2).find('.some'))
+// console.log($('.some').closest('.findme'))
+// console.log($('.some').eq(2).siblings())
+// console.log($('.findme').siblings())
+
+
 // console.log($('button').html('hellow '))
